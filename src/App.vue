65d3e42e3c7adafe3ac27ca3a0e647a5d56355d4
WaiteZQ <7776461+ziqiangwangye@user.noreply.gitee.com>
2020-07-13 23:34:03 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <m-header></m-header>
+    <tab></tab>
+    <router></router>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Mheader from "./components/m-header/m-header.vue"
+import tab from "./components/tab/tab.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    "m-header":Mheader,
+    tab
   }
-}
+};
 </script>
 
-<style>
+<style lang="stylus" scoped>
+@import "./common/stylus/variable.styl";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $color-theme;
 }
 </style>
--- src/api/recommend/getRecommendData.js
+++ src/api/recommend/getRecommendData.js
@@ -1,14 +1,27 @@
-const express = require("express");
+// const express = require("express");
 const {recommendTable} = require("./recommendTable");
-let app = express();
-app.get("/api/getRecommendData",function(req,res){
-    recommendTable.find({},{
-        __v:false,
-        _id:false
-    }).then((data)=>{
-        res.send(JSON.stringify(data))
-    }).catch((err)=>{
-        if (err) throw err;
-    })
-})
-app.listen(9527);
+// let app = express();
+// app.get("/api/getRecommendData",function(req,res){
+//     recommendTable.find({},{//查询当前所有的数据
+//         __v:false,
+//         _id:false
+//     }).then((data)=>{
+//         res.send(JSON.stringify(data))
+//     }).catch((err)=>{
+//         if (err) throw err;
+//     })
+// })
+// app.listen(1234);
+
+module.exports={
+    getRecommendData:function(req,res){
+        recommendTable.find({},{//查询当前所有的数据
+            __v:false,
+            _id:false
+        }).then((data)=>{
+            res.send(JSON.stringify(data))
+        }).catch((err)=>{
+            if (err) throw err;
+        })
+    }
+}
